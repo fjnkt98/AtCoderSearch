aebf018d293ab41ab040538e511db3e871e0b3c3
fjnkt98 <fjnkt98@gmail.com>
2022-12-17 15:29:24 +0900

    feat: データベース更新文をMERGE文へ変更
    sqlx::query!がMERGE文に対応してない？っぽいのでマクロではなく関数を使用することにした

--- crawler/src/contest/inserter.rs
+++ crawler/src/contest/inserter.rs
@@ -6,29 +6,20 @@ use sqlx::Pool;
 pub async fn insert(pool: &Pool<Postgres>, contests: &Vec<Contest>) -> Result<()> {
     let mut tx = pool.begin().await?;
 
-    let result = sqlx::query!("TRUNCATE contest CASCADE;")
-        .execute(&mut tx)
-        .await;
-
-    if let Err(e) = result {
-        tx.rollback().await?;
-        return Err(Error::new(e));
-    }
-
     for contest in contests.iter() {
-        let result = sqlx::query!(
+        let result = sqlx::query(
             r#"
-            INSERT INTO contest
-            (id, start_epoch_second, duration_second, title, rate_change, category)
-            VALUES ($1, $2, $3, $4, $5, $6);
-            "#,
-            contest.id,
-            contest.start_epoch_second,
-            contest.duration_second,
-            contest.title,
-            contest.rate_change,
-            contest.category
-        )
+            MERGE INTO contests
+            USING
+                (VALUES($1, $2, $3, $4, $5, $6)) AS contest(id, start_epoch_second, duration_second, title, rate_change, category)
+            ON
+                contests.id = contest.id
+            WHEN MATCHED THEN
+                UPDATE SET (id, start_epoch_second, duration_second, title, rate_change, category) = (contest.id, contest.start_epoch_second, contest.duration_second, contest.title, contest.rate_change, contest.category)
+            WHEN NOT MATCHED THEN
+                INSERT (id, start_epoch_second, duration_second, title, rate_change, category)
+                VALUES (contest.id, contest.start_epoch_second, contest.duration_second, contest.title, contest.rate_change, contest.category);
+            "#).bind(&contest.id).bind(contest.start_epoch_second).bind(contest.duration_second).bind(&contest.title).bind(&contest.rate_change).bind(&contest.category)
         .execute(&mut tx)
         .await;
 
