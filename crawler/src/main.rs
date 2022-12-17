f531c87860793a2c5c260c8f05348264a5003462
fjnkt98 <fjnkt98@gmail.com>
2022-12-17 15:30:45 +0900

    feat: コンテスト情報を取得してデータベースに格納する一連の処理を実装

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,11 +1,30 @@
 mod contest;
 
+use anyhow::Result;
 use contest::crawler;
+use contest::inserter;
+use contest::models::Contest;
+use dotenvy::dotenv;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+
+    let database_url: String =
+        std::env::var("DATABASE_URL").expect("DATABASE_URL must be configured.");
+
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await?;
+
     let contest_crawler = crawler::ContestCrawler::new();
+    let contests: Vec<Contest> = contest_crawler
+        .crawl()
+        .await
+        .expect("Failed to get contests.");
 
-    let contests = contest_crawler.crawl().expect("Failed to get contests.");
+    inserter::insert(&pool, &contests).await?;
 
-    println!("{:?}", contests);
+    Ok(())
 }
