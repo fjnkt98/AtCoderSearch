baf84ac67791705b9b7637e08d35abc083eae6de
fjnkt98 <fjnkt98@gmail.com>
2023-01-01 18:50:55 +0900

    feat: サンプル

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::extract::Extension;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::{Router, Server};
+use dotenvy::dotenv;
+use solr_client::client::SolrClient;
+use solr_client::core::SolrCore;
+use std::env;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tracing_subscriber::fmt;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let log_level = env::var("RUST_LOG").unwrap_or(String::from("debug"));
+    env::set_var("RUST_LOG", log_level);
+    fmt().init();
+
+    let solr = SolrClient::new("http://localhost", 8983).unwrap();
+    let core = solr.core("atcoder").await.unwrap();
+    let app = Router::new()
+        .route("/", get(index))
+        .layer(Extension(Arc::new(core)));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index(Extension(core): Extension<Arc<SolrCore>>) -> Result<impl IntoResponse, StatusCode> {
+    let status = core
+        .status()
+        .await
+        .or(Err(StatusCode::INTERNAL_SERVER_ERROR))?;
+
+    Ok((StatusCode::OK, status.name))
 }
