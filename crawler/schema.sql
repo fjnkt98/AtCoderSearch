87803f71a5528930d7ce5d306de2ba315152ce80
fjnkt98 <fjnkt98@gmail.com>
2023-01-08 00:30:46 +0900

    chore: データベーススキーマ定義変更
    文字列型はTEXT型にするのが推奨されているらしい

--- crawler/schema.sql
+++ crawler/schema.sql
@@ -1,23 +1,21 @@
 CREATE TABLE IF NOT EXISTS contests (
-    id VARCHAR(255) PRIMARY KEY,
+    id TEXT PRIMARY KEY,
     start_epoch_second BIGINT NOT NULL,
     duration_second BIGINT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    rate_change VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL
+    title TEXT NOT NULL,
+    rate_change TEXT NOT NULL,
+    category TEXT NOT NULL
 );
 
-CREATE INDEX contest_id_index ON contests (id);
-
 CREATE TABLE IF NOT EXISTS problems (
-    id VARCHAR(255) PRIMARY KEY,
-    contest_id VARCHAR(255) NOT NULL REFERENCES contests(id) ON DELETE CASCADE,
-    problem_index VARCHAR(20) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    url VARCHAR(255) NOT NULL,
+    id TEXT PRIMARY KEY,
+    contest_id TEXT NOT NULL REFERENCES contests (id) ON DELETE CASCADE,
+    problem_index TEXT NOT NULL,
+    name TEXT NOT NULL,
+    title TEXT NOT NULL,
+    url TEXT NOT NULL,
     html TEXT NOT NULL,
     difficulty INTEGER NOT NULL
 );
 
-CREATE INDEX problem_id_index ON problems (id);
+CREATE INDEX contest_id_index ON problems (contest_id);
