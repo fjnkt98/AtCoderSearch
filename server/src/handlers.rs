3f19258199be0829e11e70c03b72eac3035123ac
fjnkt98 <fjnkt98@gmail.com>
2023-01-08 12:57:30 +0900

    feat: クエリパラメータのバリデーション実装

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,6 +1,6 @@
-use crate::models::{SearchParams, ValidatedSearchParams};
+use crate::models::{SearchParams, ValidatedSearchForm, ValidatedSearchQueryParams};
 use anyhow::Result;
-use axum::extract::{Extension, Form, Query};
+use axum::extract::Extension;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::Json;
@@ -10,8 +10,8 @@ use tracing::instrument;
 
 #[instrument(skip(core))]
 pub async fn search_with_qs(
+    ValidatedSearchQueryParams(params): ValidatedSearchQueryParams<SearchParams>,
     Extension(core): Extension<Arc<SolrCore>>,
-    Query(params): Query<SearchParams>,
 ) -> Result<impl IntoResponse, StatusCode> {
     let q = params.q.unwrap_or(String::from("*"));
     let params = vec![(String::from("q"), format!("text_ja:{}", q))];
@@ -27,7 +27,7 @@ pub async fn search_with_qs(
 #[instrument(skip(core))]
 pub async fn search_with_form(
     Extension(core): Extension<Arc<SolrCore>>,
-    Form(params): Form<SearchParams>,
+    ValidatedSearchForm(params): ValidatedSearchForm<SearchParams>,
 ) -> Result<impl IntoResponse, StatusCode> {
     let q = params.q.unwrap_or(String::from("*"));
     let params = vec![(String::from("q"), format!("text_ja:{}", q))];
--- server/src/models.rs
+++ server/src/models.rs
@@ -1,17 +1,27 @@
 use axum::async_trait;
-use axum::extract::FromRequest;
+use axum::extract::{FromRequest, FromRequestParts};
 use axum::http::StatusCode;
-use axum::{BoxError, Json};
+use axum::{BoxError, Form, Json};
+use http::request::Parts;
 use http_body::Body;
 use hyper::Request;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-pub struct ValidatedSearchParams<T>(pub T);
+#[derive(Debug, Serialize, Deserialize, Clone, Validate)]
+pub struct SearchParams {
+    pub q: Option<String>,
+    #[validate(range(max = 1000))]
+    pub o: Option<u32>,
+    #[validate(range(max = 200))]
+    pub l: Option<u32>,
+}
+
+pub struct ValidatedSearchJson<T>(pub T);
 
 #[async_trait]
-impl<T, S, B> FromRequest<S, B> for ValidatedSearchParams<T>
+impl<T, S, B> FromRequest<S, B> for ValidatedSearchJson<T>
 where
     T: DeserializeOwned + Validate,
     B: Body + Send + 'static,
@@ -38,17 +48,66 @@ where
             )
         })?;
 
-        Ok(ValidatedSearchParams(value))
+        Ok(ValidatedSearchJson(value))
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, Validate)]
-pub struct SearchParams {
-    pub q: Option<String>,
-    #[validate(range(max = 1000))]
-    pub o: Option<u32>,
-    #[validate(range(max = 200))]
-    pub l: Option<u32>,
+pub struct ValidatedSearchForm<T>(pub T);
+
+#[async_trait]
+impl<T, S, B> FromRequest<S, B> for ValidatedSearchForm<T>
+where
+    T: Validate,
+    Form<T>: FromRequest<S, B>,
+    B: Send + 'static,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
+        let Form(value) = Form::<T>::from_request(req, state)
+            .await
+            .map_err(|_| (StatusCode::BAD_REQUEST, format!("Invalid format forms")))?;
+
+        value.validate().map_err(|rejection| {
+            (
+                StatusCode::BAD_REQUEST,
+                format!("Validation error: [{}]", rejection).replace('\n', ", "),
+            )
+        })?;
+
+        Ok(ValidatedSearchForm(value))
+    }
+}
+
+pub struct ValidatedSearchQueryParams<T>(pub T);
+
+#[async_trait]
+impl<T, S> FromRequestParts<S> for ValidatedSearchQueryParams<T>
+where
+    T: DeserializeOwned + Validate,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        let query = parts.uri.query().unwrap_or_default();
+        let value: T = serde_urlencoded::from_str(query).map_err(|rejection| {
+            (
+                StatusCode::BAD_REQUEST,
+                format!("Invalid format query string: [{}]", rejection),
+            )
+        })?;
+
+        value.validate().map_err(|rejection| {
+            (
+                StatusCode::BAD_REQUEST,
+                format!("Validation error: [{}]", rejection).replace('\n', ", "),
+            )
+        })?;
+
+        Ok(ValidatedSearchQueryParams(value))
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
