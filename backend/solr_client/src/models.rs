bd7b252bdae91a05c02b3ccd1860d96174434c90
fjnkt98 <fjnkt98@gmail.com>
2023-01-26 22:53:19 +0900

    feat: レンジファセットの結果をうまくデシリアライズする処理を追加
    テストはこれから書く

--- backend/solr_client/src/models.rs
+++ backend/solr_client/src/models.rs
@@ -1,10 +1,17 @@
+/// 命名規則
+///
+/// - Solrから返ってくるレスポンス本体のモデル -> SolrXXXXResponse
+/// - レスポンスの一部 -> SolrXXXX(Header|Body|Info|e.t.c)
+///
+use chrono::{DateTime, FixedOffset, Utc};
 use itertools::Itertools;
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::de::{Error, Unexpected};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde_json::Value;
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct ResponseHeader {
+pub struct SolrResponseHeader {
     pub status: u32,
     #[serde(alias = "QTime")]
     pub qtime: u32,
@@ -33,7 +40,7 @@ pub struct LuceneInfo {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SolrSystemInfo {
     #[serde(alias = "responseHeader")]
-    pub header: ResponseHeader,
+    pub header: SolrResponseHeader,
     pub mode: String,
     pub solr_home: String,
     pub core_root: String,
@@ -71,7 +78,7 @@ pub struct IndexInfo {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct CoreStatus {
+pub struct SolrCoreStatus {
     pub name: String,
     #[serde(alias = "instanceDir")]
     pub instance_dir: String,
@@ -88,10 +95,10 @@ pub struct CoreStatus {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SolrCoreList {
     #[serde(alias = "responseHeader")]
-    pub header: ResponseHeader,
+    pub header: SolrResponseHeader,
     #[serde(alias = "initFailures")]
     pub init_failures: Value,
-    pub status: Option<HashMap<String, CoreStatus>>,
+    pub status: Option<HashMap<String, SolrCoreStatus>>,
     pub error: Option<SolrErrorInfo>,
 }
 
@@ -108,35 +115,37 @@ impl SolrCoreList {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SolrSimpleResponse {
     #[serde(alias = "responseHeader")]
-    pub header: ResponseHeader,
+    pub header: SolrResponseHeader,
     pub error: Option<SolrErrorInfo>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SolrSelectResponse {
     #[serde(alias = "responseHeader")]
-    pub header: ResponseHeader,
-    pub response: SolrSelectResponseBody,
-    pub facet_counts: Option<FacetResult>,
+    pub header: SolrResponseHeader,
+    pub response: SolrSelectBody,
+    pub facet_counts: Option<SolrFacetBody>,
     pub error: Option<SolrErrorInfo>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct SolrSelectResponseBody {
+pub struct SolrSelectBody {
     #[serde(alias = "numFound")]
     pub num_found: u32,
     pub start: u32,
     #[serde(alias = "numFoundExact")]
     pub num_found_exact: bool,
+    // TODO: ジェネリクス化
     pub docs: Value,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct FacetResult {
+pub struct SolrFacetBody {
     pub facet_queries: Value,
     #[serde(deserialize_with = "deserialize_facet_fields")]
     pub facet_fields: HashMap<String, Vec<(String, u32)>>,
-    pub facet_ranges: HashMap<String, RangeFacet>,
+    #[serde(deserialize_with = "deserialize_facet_ranges")]
+    pub facet_ranges: HashMap<String, RangeFacetKind>,
     pub facet_intervals: Value,
     pub facet_heatmaps: Value,
 }
@@ -169,18 +178,198 @@ where
     Ok(value)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct RangeFacet {
-    #[serde(deserialize_with = "deserialize_range_facet_counts")]
-    pub counts: Vec<(String, u32)>,
-    pub gap: Value,
-    pub start: Value,
-    pub end: Value,
-    pub before: Option<Value>,
-    pub after: Option<Value>,
-    pub between: Option<Value>,
+/// レンジファセットの結果をデシリアライズする関数
+///
+/// レンジファセットの結果の型はフィールドの型依存なのでアドホックに場合分けする必要があった
+fn deserialize_facet_ranges<'de, D>(
+    deserializer: D,
+) -> Result<HashMap<String, RangeFacetKind>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value: HashMap<String, Value> = Deserialize::deserialize(deserializer)?;
+    let mut result: HashMap<String, RangeFacetKind> = HashMap::new();
+    for (field, value) in value.iter() {
+        match &value["start"] {
+            Value::Number(start) => {
+                if start.is_i64() {
+                    let value: IntegerRangeFacet =
+                        serde_json::from_value(value.clone()).map_err(|e| {
+                            D::Error::custom(format!(
+                                "Failed to parse integer range facet result. [{}]",
+                                e.to_string()
+                            ))
+                        })?;
+                    result.insert(field.to_string(), RangeFacetKind::Integer(value));
+                } else {
+                    let value: FloatRangeFacet =
+                        serde_json::from_value(value.clone()).map_err(|e| {
+                            D::Error::custom(format!(
+                                "Failed to parse float range facet result. [{}]",
+                                e.to_string()
+                            ))
+                        })?;
+                    result.insert(field.to_string(), RangeFacetKind::Float(value));
+                }
+            }
+            Value::String(start) => {
+                if DateTime::parse_from_rfc3339(&start.replace("Z", "+00:00")).is_ok() {
+                    let value: DateTimeRangeFacet =
+                        serde_json::from_value(value.clone()).map_err(|e| {
+                            D::Error::custom(format!(
+                                "Failed to parse datetime range facet result. [{}]",
+                                e.to_string()
+                            ))
+                        })?;
+                    result.insert(field.to_string(), RangeFacetKind::DateTime(value));
+                } else {
+                    // TODO; 数値、日付型以外のレンジファセットがあったら処理を追加する
+                    return Err(D::Error::custom("Unexpected range facet value type."));
+                }
+            }
+            _ => {
+                return Err(D::Error::custom("Mismatched range facet value type."));
+            }
+        }
+    }
+    Ok(result)
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub enum RangeFacetKind {
+    Integer(IntegerRangeFacet),
+    Float(FloatRangeFacet),
+    DateTime(DateTimeRangeFacet),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IntegerRangeFacet {
+    #[serde(deserialize_with = "deserialize_range_facet_counts")]
+    pub counts: Vec<(String, u32)>,
+    pub gap: i64,
+    pub start: i64,
+    pub end: i64,
+    pub before: Option<i64>,
+    pub after: Option<i64>,
+    pub between: Option<i64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FloatRangeFacet {
+    #[serde(deserialize_with = "deserialize_range_facet_counts")]
+    pub counts: Vec<(String, u32)>,
+    pub gap: f64,
+    pub start: f64,
+    pub end: f64,
+    pub before: Option<f64>,
+    pub after: Option<f64>,
+    pub between: Option<f64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DateTimeRangeFacet {
+    #[serde(deserialize_with = "deserialize_range_facet_counts")]
+    pub counts: Vec<(String, u32)>,
+    #[serde(
+        serialize_with = "serialize_datetime",
+        deserialize_with = "deserialize_datetime"
+    )]
+    pub gap: DateTime<FixedOffset>,
+    #[serde(
+        serialize_with = "serialize_datetime",
+        deserialize_with = "deserialize_datetime"
+    )]
+    pub start: DateTime<FixedOffset>,
+    #[serde(
+        serialize_with = "serialize_datetime",
+        deserialize_with = "deserialize_datetime"
+    )]
+    pub end: DateTime<FixedOffset>,
+    #[serde(
+        serialize_with = "serialize_optional_datetime",
+        deserialize_with = "deserialize_optional_datetime"
+    )]
+    pub before: Option<DateTime<FixedOffset>>,
+    #[serde(
+        serialize_with = "serialize_optional_datetime",
+        deserialize_with = "deserialize_optional_datetime"
+    )]
+    pub after: Option<DateTime<FixedOffset>>,
+    #[serde(
+        serialize_with = "serialize_optional_datetime",
+        deserialize_with = "deserialize_optional_datetime"
+    )]
+    pub between: Option<DateTime<FixedOffset>>,
+}
+
+/// DateTime型をSolrが扱える文字列形式にシリアライズする関数
+fn serialize_datetime<S>(timestamp: &DateTime<FixedOffset>, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    s.serialize_str(
+        &timestamp
+            .with_timezone(&Utc)
+            .to_rfc3339()
+            .replace("+00:00", "Z"),
+    )
+}
+
+/// DateTime型をSolrが扱える文字列形式にシリアライズする関数(Option版)
+/// TODO: コードの重複が多いのでスマートに解決する方法を探す
+fn serialize_optional_datetime<S>(
+    timestamp: &Option<DateTime<FixedOffset>>,
+    s: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    match timestamp {
+        Some(timestamp) => s.serialize_str(
+            &timestamp
+                .with_timezone(&Utc)
+                .to_rfc3339()
+                .replace("+00:00", "Z"),
+        ),
+        None => s.serialize_none(),
+    }
+}
+
+/// Solrのタイムスタンプ文字列表現をDateTime型にデシリアライズする関数
+fn deserialize_datetime<'de, D>(deserializer: D) -> Result<DateTime<FixedOffset>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = String::deserialize(deserializer)?;
+    if let Ok(timestamp) = DateTime::parse_from_rfc3339(&value.replace("Z", "+00:00")) {
+        return Ok(timestamp);
+    } else {
+        return Err(Error::invalid_value(
+            Unexpected::Str(&value),
+            &"Invalid timestamp string",
+        ));
+    }
+}
+
+/// Solrのタイムスタンプ文字列表現をDateTime型にデシリアライズする関数(Option版)
+/// TODO: コードの重複が多いのでスマートに解決する方法を探す
+fn deserialize_optional_datetime<'de, D>(
+    deserializer: D,
+) -> Result<Option<DateTime<FixedOffset>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = String::deserialize(deserializer)?;
+    if let Ok(timestamp) = DateTime::parse_from_rfc3339(&value.replace("Z", "+00:00")) {
+        return Ok(Some(timestamp));
+    } else {
+        return Ok(None);
+    }
+}
+
+/// ファセットの結果の配列をRustが扱える配列にデシリアライズする関数
+///
+/// Solrのファセットの結果は「文字列、数値」が交互に格納された配列で返ってくる。Rustは型が混じった配列を扱えないので、タプルのリストに変換する。
 fn deserialize_range_facet_counts<'de, D>(deserializer: D) -> Result<Vec<(String, u32)>, D::Error>
 where
     D: Deserializer<'de>,
@@ -215,7 +404,13 @@ pub struct SolrAnalysisField {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SolrAnalysisResponse {
     #[serde(alias = "responseHeader")]
-    pub header: ResponseHeader,
+    pub header: SolrResponseHeader,
     pub analysis: SolrAnalysisBody,
     pub error: Option<SolrErrorInfo>,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    // TODO: テスト書く
 }
