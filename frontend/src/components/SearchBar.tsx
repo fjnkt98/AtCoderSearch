a50b4219989c6da5d37e56481afcbc705fd1c95a
fjnkt98 <fjnkt98@gmail.com>
2023-03-05 17:19:37 +0900

    feat: 検索実行機能をリファクタ

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,18 +1,49 @@
-import { useState, ChangeEvent } from "react";
-import { Link, createSearchParams } from "react-router-dom";
+import { useState, ChangeEvent, KeyboardEvent } from "react";
+import { createSearchParams, useNavigate } from "react-router-dom";
 
 export function SearchBar() {
+  // 検索キーワード
   const [text, setText] = useState<string>("");
-  const [url, setUrl] = useState<string>("/search");
+  // 検索用パラメータ
+  // キーワード検索を実行したときの初期ページは1固定
+  const [params, setParams] = useState<URLSearchParams>(
+    createSearchParams({
+      c: "20",
+      p: "1",
+    })
+  );
 
+  // 検索ボックスの入力を更新したときの処理
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setText(() => {
-      return e.target.value;
+    setText(e.target.value);
+
+    // 入力の度に検索用パラメータを更新する
+    setParams((params) => {
+      params.set("q", e.target.value);
+      return params;
     });
-    const params: string = createSearchParams({
-      q: e.target.value,
-    }).toString();
-    setUrl(`/search?${params}`);
+  };
+
+  // ページ遷移のためのオブジェクト
+  const navigate = useNavigate();
+
+  // 検索結果ページへ遷移する関数
+  const search = () => {
+    // 空白文字だけの場合は検索を実行しない
+    if (text.trim() === "") {
+      return;
+    }
+
+    // 検索APIは検索結果ページに遷移して初めて実行される
+    navigate(`/search?${params.toString()}`);
+  };
+
+  // エンターキーで検索を実行するためのハンドラ
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      search();
+    }
   };
 
   return (
@@ -21,9 +52,10 @@ export function SearchBar() {
         type="text"
         placeholder="Search Problems"
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       ></input>
-      <button type="button">
-        <Link to={url}>Search</Link>
+      <button type="button" onClick={search}>
+        Search
       </button>
     </div>
   );
