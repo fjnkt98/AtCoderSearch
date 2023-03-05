a12364bcd7ca25319f511899242a13752e784249
fjnkt98 <fjnkt98@gmail.com>
2023-03-05 17:49:45 +0900

    feat: ソート順切り替え仮実装

--- frontend/src/components/SortOrder.tsx
+++ frontend/src/components/SortOrder.tsx
@@ -1,3 +1,23 @@
+import { ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function SortOrder() {
-  return <div>SortOrder</div>;
+  const navigate = useNavigate();
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    navigate(`/search?q=atcoder&s=${e.target.value}`);
+  };
+
+  return (
+    <div className="relative w-full lg:max-w-sm">
+      <select
+        onChange={handleChange}
+        className="w-full appearance-none rounded-md border bg-white p-2.5 text-gray-500 shadow-sm outline-none focus:border-indigo-600"
+      >
+        <option value="-score">デフォルト</option>
+        <option value="difficulty">難易度(低い順)</option>
+        <option value="-difficulty">難易度(高い順)</option>
+      </select>
+    </div>
+  );
 }
