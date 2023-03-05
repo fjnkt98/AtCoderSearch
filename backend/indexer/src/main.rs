fadbdf310373287c67cd007b3685948996275b02
fjnkt98 <fjnkt98@gmail.com>
2023-03-05 12:31:21 +0900

    chore: 不要になったコードを削除

--- backend/indexer/src/main.rs
+++ backend/indexer/src/main.rs
@@ -181,43 +181,5 @@ async fn main() -> Result<()> {
         }
     }
 
-    // let log_level = env::var("RUST_LOG").unwrap_or(String::from("info"));
-    // env::set_var("RUST_LOG", "info");
-
-    // let filter =
-    //     EnvFilter::try_from_default_env()?.add_directive(format!("indexer={}", log_level).parse()?);
-    // fmt().with_env_filter(filter).init();
-
-    // let database_url: String = env::var("DATABASE_URL").expect("DATABASE_URL must be configured.");
-
-    // let pool: Pool<Postgres> = sqlx::postgres::PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect(&database_url)
-    //     .await?;
-
-    // let solr_host = env::var("SOLR_HOST").unwrap_or(String::from("http://localhost"));
-    // let solr_port = env::var("SOLR_PORT")
-    //     .map(|v| v.parse::<u32>().unwrap())
-    //     .unwrap_or(8983u32);
-
-    // let core_name = env::var("CORE_NAME").expect("CORE_NAME must be configured");
-
-    // let solr = SolrClient::new(&solr_host, solr_port).expect("Failed to create solr client.");
-    // let core = solr
-    //     .core(&core_name)
-    //     .await
-    //     .expect("Failed to create core client");
-
-    // let manager = IndexingManager::new(&pool, core);
-    // manager
-    //     .write()
-    //     .await
-    //     .expect("Failed to write JSON document.");
-
-    // manager
-    //     .post()
-    //     .await
-    //     .expect("Failed to post document to solr.");
-
     Ok(())
 }
