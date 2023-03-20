0b9419665fb9f9be5cc9ac267f3ac6ccd0226c69
fjnkt98 <fjnkt98@gmail.com>
2023-03-20 22:37:28 +0900

    feat: リロードしたときにソート順セレクタに反映されるようにした

--- frontend/src/components/SortOrder.tsx
+++ frontend/src/components/SortOrder.tsx
@@ -1,12 +1,25 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { searchParamsStateSelector } from "../libs/searchParamsState";
 
-export function SortOrder() {
-  const searchParams = useRecoilValue(searchParamsStateSelector);
+const sortKeyMapping = new Map<string, string>([
+  ["-score", "関連度順"],
+  ["difficulty", "難易度(低い順)"],
+  ["-difficulty", "難易度(高い順)"],
+  ["start_at", "開催時期(早い順)"],
+  ["-start_at", "開催時期(遅い順)"],
+]);
 
+export function SortOrder() {
   const navigate = useNavigate();
+  const searchParams = useRecoilValue(searchParamsStateSelector);
+  const [selected, setSelected] = useState<string | null>(
+    searchParams.get("sort") ?? ""
+  );
+  useEffect(() => {
+    setSelected(searchParams.get("sort") ?? "-score");
+  }, [searchParams]);
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const params = new URLSearchParams(searchParams);
@@ -22,11 +35,12 @@ export function SortOrder() {
         onChange={handleChange}
         className="w-full appearance-none rounded-md border bg-white p-2.5 text-gray-900 shadow-sm outline-none focus:border-indigo-600"
       >
-        <option value="-score">関連度順</option>
-        <option value="difficulty">難易度(低い順)</option>
-        <option value="-difficulty">難易度(高い順)</option>
-        <option value="start_at">開催時期(早い順)</option>
-        <option value="-start_at">開催時期(遅い順))</option>
+        <option hidden>{sortKeyMapping.get(selected ?? "-score")}</option>
+        <option value="-score">{sortKeyMapping.get("-score")}</option>
+        <option value="difficulty">{sortKeyMapping.get("difficulty")}</option>
+        <option value="-difficulty">{sortKeyMapping.get("-difficulty")}</option>
+        <option value="start_at">{sortKeyMapping.get("start_at")}</option>
+        <option value="-start_at">{sortKeyMapping.get("-start_at")}</option>
       </select>
     </div>
   );
