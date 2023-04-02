ce4b4f17fe542d90c56adad51b0ad0097d6af066
fjnkt98 <fjnkt98@gmail.com>
2023-04-02 18:50:28 +0900

    feat: 検索結果の統計表示をProblemListコンポーネントに統合

--- frontend/src/components/ProblemList.tsx
+++ frontend/src/components/ProblemList.tsx
@@ -1,12 +1,23 @@
 import { Problem } from "./Problem";
 import { useRecoilValue } from "recoil";
-import { searchResponseItemsSelector } from "../libs/searchResponseState";
+import {
+  searchResponseItemsSelector,
+  searchResponseTotalSelector,
+  searchResponseCountSelector,
+  searchResponseTimeSelector,
+} from "../libs/searchResponseState";
 
 export function ProblemList() {
   const items = useRecoilValue(searchResponseItemsSelector);
+  const total = useRecoilValue(searchResponseTotalSelector);
+  const count = useRecoilValue(searchResponseCountSelector);
+  const time = useRecoilValue(searchResponseTimeSelector);
 
   return (
-    <div>
+    <div className="">
+      <span className="inline-block px-4 text-slate-400">
+        {count}件/{total}件 約{time / 1000}秒
+      </span>
       {items.map((item) => {
         return <Problem key={item.problem_id} item={item} />;
       })}
