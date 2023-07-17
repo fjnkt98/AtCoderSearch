bf8c3f0f79d561ad5d60d11ca2fbe2c8ae216a2b
fjnkt98 <fjnkt98@gmail.com>
2023-07-17 18:53:02 +0900

    add process to create directory when it doesn't exist

--- backend/cmd/generate.go
+++ backend/cmd/generate.go
@@ -1,25 +1,43 @@
 package cmd
 
 import (
+	"errors"
 	"fjnkt98/atcodersearch/atcodersearch/problem"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/spf13/cobra"
 )
 
-func getSaveDir(cmd *cobra.Command) string {
+func getSaveDir(cmd *cobra.Command, domain string) (string, error) {
 	var saveDir string
-	if s, err := cmd.Flags().GetString("save-dir"); err != nil {
-		saveDir = s
+	if s, err := cmd.Flags().GetString("save-dir"); err == nil && s != "" {
+		return s, nil
 	} else if dir := os.Getenv("DOCUMENT_SAVE_DIRECTORY"); dir != "" {
 		saveDir = dir
 	} else {
-		log.Fatal("couldn't determine document save directory")
+		return "", errors.New("couldn't determine document save directory")
 	}
 
-	return saveDir
+	saveDir = filepath.Join(saveDir, domain)
+	if _, err := os.Stat(saveDir); err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("The directory `%s` doesn't exists, so attempt to create it.", saveDir)
+			if err := os.Mkdir(saveDir, os.ModePerm); err != nil {
+				return "", fmt.Errorf("failed to create directory `%s`: %s", saveDir, err.Error())
+			} else {
+				log.Printf("The directory `%s` was successfully created.", saveDir)
+				return saveDir, nil
+			}
+		} else {
+			return "", fmt.Errorf("failed to get stat directory `%s`: %s", saveDir, err.Error())
+		}
+	} else {
+		return saveDir, nil
+	}
 }
 
 func getDB() *sqlx.DB {
@@ -42,7 +60,12 @@ var generateProblemCmd = &cobra.Command{
 	Short: "Generate problem document JSON files",
 	Long:  "Generate problem document JSON files",
 	Run: func(cmd *cobra.Command, args []string) {
-		generator := problem.NewProblemDocumentGenerator(getDB(), getSaveDir(cmd))
+		saveDir, err := getSaveDir(cmd, "problem")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		generator := problem.NewProblemDocumentGenerator(getDB(), saveDir)
 		if err := generator.Run(1000, 10); err != nil {
 			log.Fatal(err.Error())
 		}
