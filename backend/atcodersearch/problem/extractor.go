2e1ce5d7f7472d89691fd44692ac9e7d1703dbdd
fjnkt98 <fjnkt98@gmail.com>
2023-07-19 23:26:14 +0900

    fix extractor implementation

--- backend/atcodersearch/problem/extractor.go
+++ backend/atcodersearch/problem/extractor.go
@@ -14,47 +14,30 @@ func NewFullTextExtractor() FullTextExtractor {
 	return FullTextExtractor{}
 }
 
-func (f *FullTextExtractor) DFS(element *goquery.Selection) string {
-	buffer := make([]string, 0)
-
-	element.Children().Each(func(_ int, e *goquery.Selection) {
-		switch goquery.NodeName(e) {
-		case "pre", "h3", "var":
-		default:
-			buffer = append(buffer, e.Text())
-			buffer = append(buffer, f.DFS(e))
-		}
-	})
-
-	return strings.Join(buffer, "")
-}
-
 func (f *FullTextExtractor) Extract(html io.Reader) ([]string, []string, error) {
 	doc, _ := goquery.NewDocumentFromReader(html)
 
 	textJa := make([]string, 0)
 	textEn := make([]string, 0)
 
-	if ja := doc.Find("span.lang-ja"); ja != nil {
-		ja.Find("section").Each(func(_ int, section *goquery.Selection) {
-			if h3 := section.Find("h3").Text(); strings.Contains(h3, "問題") {
-				textJa = append(textJa, f.DFS(section))
-			}
-		})
-	} else {
-		doc.Find("section").Each(func(_ int, section *goquery.Selection) {
-			if h3 := section.Find("h3").Text(); strings.Contains(h3, "問題") {
-				textJa = append(textJa, f.DFS(section))
-			}
-		})
-	}
+	doc.Find("section").Each(func(_ int, section *goquery.Selection) {
+		// For modern contest problem format
+		if strings.Contains(section.Find("h3").Text(), "問題") {
+			textJa = append(textJa, strings.Split(section.Text(), "\n")...)
+		}
 
-	if en := doc.Find("span.lang-en"); en != nil {
-		en.Find("section").Each(func(_ int, section *goquery.Selection) {
-			if h3 := section.Find("h3").Text(); strings.Contains(h3, "Statement") {
-				textEn = append(textEn, f.DFS(section))
+		// For legacy contest problem format
+		if prev := section.Prev(); goquery.NodeName(prev) == "h3" {
+			if text := prev.Text(); strings.Contains(text, "問題") {
+				textJa = append(textJa, strings.Split(section.Text(), "\n")...)
 			}
-		})
-	}
+		}
+	})
+
+	doc.Find("span.lang-en").Find("section").Each(func(_ int, section *goquery.Selection) {
+		if strings.Contains(section.NextAll().Find("h3").Text(), "Statement") || strings.Contains(section.Find("h3").Text(), "Statement") {
+			textEn = append(textEn, strings.Split(section.Text(), "\n")...)
+		}
+	})
 	return textJa, textEn, nil
 }
