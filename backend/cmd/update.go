fbf71a6cfcf87350847301022fec3b470b98413b
fjnkt98 <fjnkt98@gmail.com>
2023-08-06 20:28:10 +0900

    change update command architecture

--- backend/cmd/update.go
+++ backend/cmd/update.go
@@ -4,10 +4,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fjnkt98/atcodersearch/acs"
 	"fjnkt98/atcodersearch/problem"
 	"fjnkt98/atcodersearch/solr"
 	"fjnkt98/atcodersearch/user"
+	"fmt"
 	"log"
 	"os"
 
@@ -26,61 +28,44 @@ var updateProblemCmd = &cobra.Command{
 	Short: "update problem index",
 	Long:  "update problem index",
 	Run: func(cmd *cobra.Command, args []string) {
-		saveDir, err := GetSaveDir(cmd, "problem")
-		if err != nil {
+		type Options struct {
+			SaveDir            string `json:"save-dir"`
+			SkipFetch          bool   `json:"skip-fetch"`
+			Optimize           bool   `json:"optimize"`
+			ChunkSize          int    `json:"chunk-size"`
+			GenerateConcurrent int    `json:"generate-concurrent"`
+			PostConcurrent     int    `json:"post-concurrent"`
+			Duration           int    `json:"duration"`
+			All                bool   `json:"all"`
+		}
+
+		opt := Options{}
+		var err error
+
+		if opt.SaveDir, err = GetSaveDir(cmd, "problem"); err != nil {
 			log.Fatal(err.Error())
 		}
-
-		db := GetDB()
-		skipFetch, err := cmd.Flags().GetBool("skip-fetch")
-		if err != nil {
+		if opt.SkipFetch, err = cmd.Flags().GetBool("skip-fetch"); err != nil {
 			log.Fatalf("failed to get flag `--skip-fetch`: %s", err.Error())
 		}
-
-		if !skipFetch {
-			contestCrawler := problem.NewContestCrawler(db)
-			if err := contestCrawler.Run(); err != nil {
-				log.Fatalf("failed to save contest information: %s", err.Error())
-			}
-
-			difficultyCrawler := problem.NewDifficultyCrawler(db)
-			if err := difficultyCrawler.Run(); err != nil {
-				log.Fatalf("failed to save difficulty information: %s", err.Error())
-			}
-
-			all, err := cmd.Flags().GetBool("all")
-			if err != nil {
-				log.Fatalf("failed to get flag `all`: %s", err.Error())
-			}
-			duration, err := cmd.Flags().GetInt("duration")
-			if err != nil {
-				log.Fatalf("failed to get flag `duration`: %s", err.Error())
-			}
-			problemCrawler := problem.NewProblemCrawler(db)
-			if err := problemCrawler.Run(all, duration); err != nil {
-				log.Fatalf("failed to save problem information: %s", err.Error())
-			}
+		if opt.Optimize, err = cmd.Flags().GetBool("optimize"); err != nil {
+			log.Fatalf("failed to get value of `optimize` flag: %s", err.Error())
 		}
-
-		generator := problem.NewDocumentGenerator(db, saveDir)
-		chunkSize, err := cmd.Flags().GetInt("chunk-size")
-		if err != nil {
+		if opt.ChunkSize, err = cmd.Flags().GetInt("chunk-size"); err != nil {
 			log.Fatalf("failed to get flag `--chunk-size`: %s", err.Error())
 		}
-		generateConcurrency, err := cmd.Flags().GetInt("generate-concurrent")
-		if err != nil {
+		if opt.GenerateConcurrent, err = cmd.Flags().GetInt("generate-concurrent"); err != nil {
 			log.Fatalf("failed to get flag `--generate-concurrent`: %s", err.Error())
 		}
-
-		if err := generator.Run(chunkSize, generateConcurrency); err != nil {
-			log.Fatalf("failed to generate document: %s", err.Error())
-		}
-
-		postConcurrency, err := cmd.Flags().GetInt("post-concurrent")
-		if err != nil {
+		if opt.PostConcurrent, err = cmd.Flags().GetInt("post-concurrent"); err != nil {
 			log.Fatalf("failed to get flag `--post-concurrent`: %s", err.Error())
 		}
-
+		if opt.Duration, err = cmd.Flags().GetInt("duration"); err != nil {
+			log.Fatalf("failed to get flag `duration`: %s", err.Error())
+		}
+		if opt.All, err = cmd.Flags().GetBool("all"); err != nil {
+			log.Fatalf("failed to get flag `all`: %s", err.Error())
+		}
 		solrURL := os.Getenv("SOLR_HOST")
 		if solrURL == "" {
 			log.Fatalln("environment variable `SOLR_HOST` must be set.")
@@ -90,13 +75,54 @@ var updateProblemCmd = &cobra.Command{
 			log.Fatalf("failed to create `problem` core: %s", err.Error())
 		}
 
-		uploader := acs.NewDefaultDocumentUploader(core, saveDir)
-		optimize, err := cmd.Flags().GetBool("optimize")
-		if err != nil {
-			log.Fatalf("failed to get value of `optimize` flag: %s", err.Error())
+		db := GetDB()
+
+		f := func() error {
+			if !opt.SkipFetch {
+				contestCrawler := problem.NewContestCrawler(db)
+				if err := contestCrawler.Run(); err != nil {
+					return fmt.Errorf("failed to save contest information: %w", err)
+				}
+
+				difficultyCrawler := problem.NewDifficultyCrawler(db)
+				if err := difficultyCrawler.Run(); err != nil {
+					return fmt.Errorf("failed to save difficulty information: %w", err)
+				}
+
+				problemCrawler := problem.NewProblemCrawler(db)
+				if err := problemCrawler.Run(opt.All, opt.Duration); err != nil {
+					return fmt.Errorf("failed to save problem information: %w", err)
+				}
+			}
+
+			generator := problem.NewDocumentGenerator(db, opt.SaveDir)
+			if err := generator.Run(opt.ChunkSize, opt.GenerateConcurrent); err != nil {
+				return fmt.Errorf("failed to generate document: %w", err)
+			}
+
+			uploader := acs.NewDefaultDocumentUploader(core, opt.SaveDir)
+			if err := uploader.PostDocument(opt.Optimize, opt.PostConcurrent); err != nil {
+				return fmt.Errorf("failed to post documents: %w", err)
+			}
+			return nil
 		}
-		if err := uploader.PostDocument(optimize, postConcurrency); err != nil {
-			log.Fatalf("failed to post documents: %s", err.Error())
+
+		options, err := json.Marshal(opt)
+		if err != nil {
+			log.Fatalf("failed to marshal update options: %s", err.Error())
+		}
+
+		history := NewUpdateHistory(db, "problem", string(options))
+		if err := f(); err == nil {
+			if err := history.Finish(); err != nil {
+				log.Fatalf("failed to save update history: %s", err.Error())
+			}
+			log.Print("problem update successfully finished.")
+		} else {
+			if err := history.Cancel(); err != nil {
+				log.Printf("failed to save update history: %s", err.Error())
+			}
+			log.Fatalf("problem update failed: %s", err.Error())
 		}
 	},
 }
@@ -106,47 +132,40 @@ var updateUserCmd = &cobra.Command{
 	Short: "update user index",
 	Long:  "update user index",
 	Run: func(cmd *cobra.Command, args []string) {
-		saveDir, err := GetSaveDir(cmd, "user")
-		if err != nil {
+		type Options struct {
+			SaveDir            string `json:"save-dir"`
+			SkipFetch          bool   `json:"skip-fetch"`
+			Optimize           bool   `json:"optimize"`
+			ChunkSize          int    `json:"chunk-size"`
+			GenerateConcurrent int    `json:"generate-concurrent"`
+			PostConcurrent     int    `json:"post-concurrent"`
+			Duration           int    `json:"duration"`
+		}
+
+		opt := Options{}
+		var err error
+
+		if opt.SaveDir, err = GetSaveDir(cmd, "user"); err != nil {
 			log.Fatal(err.Error())
 		}
-
-		db := GetDB()
-		skipFetch, err := cmd.Flags().GetBool("skip-fetch")
-		if err != nil {
+		if opt.SkipFetch, err = cmd.Flags().GetBool("skip-fetch"); err != nil {
 			log.Fatalf("failed to get flag `--skip-fetch`: %s", err.Error())
 		}
-
-		if !skipFetch {
-			duration, err := cmd.Flags().GetInt("duration")
-			if err != nil {
-				log.Fatalf("failed to get flag `duration`: %s", err.Error())
-			}
-			crawler := user.NewUserCrawler(db)
-			if err := crawler.Run(duration); err != nil {
-				log.Fatalf("failed to save user information: %s", err.Error())
-			}
+		if opt.Optimize, err = cmd.Flags().GetBool("optimize"); err != nil {
+			log.Fatalf("failed to get value of `optimize` flag: %s", err.Error())
 		}
-
-		generator := user.NewDocumentGenerator(db, saveDir)
-		chunkSize, err := cmd.Flags().GetInt("chunk-size")
-		if err != nil {
+		if opt.ChunkSize, err = cmd.Flags().GetInt("chunk-size"); err != nil {
 			log.Fatalf("failed to get flag `--chunk-size`: %s", err.Error())
 		}
-		generateConcurrency, err := cmd.Flags().GetInt("generate-concurrent")
-		if err != nil {
+		if opt.GenerateConcurrent, err = cmd.Flags().GetInt("generate-concurrent"); err != nil {
 			log.Fatalf("failed to get flag `--generate-concurrent`: %s", err.Error())
 		}
-
-		if err := generator.Run(chunkSize, generateConcurrency); err != nil {
-			log.Fatalf("failed to generate document: %s", err.Error())
-		}
-
-		postConcurrency, err := cmd.Flags().GetInt("post-concurrent")
-		if err != nil {
+		if opt.PostConcurrent, err = cmd.Flags().GetInt("post-concurrent"); err != nil {
 			log.Fatalf("failed to get flag `--post-concurrent`: %s", err.Error())
 		}
-
+		if opt.Duration, err = cmd.Flags().GetInt("duration"); err != nil {
+			log.Fatalf("failed to get flag `duration`: %s", err.Error())
+		}
 		solrURL := os.Getenv("SOLR_HOST")
 		if solrURL == "" {
 			log.Fatalln("environment variable `SOLR_HOST` must be set.")
@@ -156,13 +175,44 @@ var updateUserCmd = &cobra.Command{
 			log.Fatalf("failed to create `user` core: %s", err.Error())
 		}
 
-		uploader := acs.NewDefaultDocumentUploader(core, saveDir)
-		optimize, err := cmd.Flags().GetBool("optimize")
-		if err != nil {
-			log.Fatalf("failed to get value of `optimize` flag: %s", err.Error())
+		db := GetDB()
+
+		f := func() error {
+			if !opt.SkipFetch {
+				crawler := user.NewUserCrawler(db)
+				if err := crawler.Run(opt.Duration); err != nil {
+					return fmt.Errorf("failed to save user information: %w", err)
+				}
+			}
+
+			generator := user.NewDocumentGenerator(db, opt.SaveDir)
+			if err := generator.Run(opt.ChunkSize, opt.GenerateConcurrent); err != nil {
+				return fmt.Errorf("failed to generate document: %w", err)
+			}
+
+			uploader := acs.NewDefaultDocumentUploader(core, opt.SaveDir)
+			if err := uploader.PostDocument(opt.Optimize, opt.PostConcurrent); err != nil {
+				return fmt.Errorf("failed to post documents: %w", err)
+			}
+			return nil
 		}
-		if err := uploader.PostDocument(optimize, postConcurrency); err != nil {
-			log.Fatalf("failed to post documents: %s", err.Error())
+
+		options, err := json.Marshal(opt)
+		if err != nil {
+			log.Fatalf("failed to marshal update options: %s", err.Error())
+		}
+
+		history := NewUpdateHistory(db, "user", string(options))
+		if err := f(); err == nil {
+			if err := history.Finish(); err != nil {
+				log.Fatalf("failed to save update history: %s", err.Error())
+			}
+			log.Print("user update successfully finished.")
+		} else {
+			if err := history.Cancel(); err != nil {
+				log.Printf("failed to save update history: %s", err.Error())
+			}
+			log.Fatalf("user update failed: %s", err.Error())
 		}
 	},
 }
@@ -170,15 +220,13 @@ var updateUserCmd = &cobra.Command{
 func init() {
 	updateCmd.PersistentFlags().String("save-dir", "", "Directory path at which generated documents will be saved.")
 	updateCmd.PersistentFlags().BoolP("skip-fetch", "f", false, "Skip crawling if true.")
-	updateCmd.PersistentFlags().BoolP("optimize", "o", false, "Optimize index if true.")
+	updateCmd.PersistentFlags().BoolP("optimize", "o", true, "Optimize index if true.")
 	updateCmd.PersistentFlags().Int("chunk-size", 1000, "Number of documents to write in 1 file.")
 	updateCmd.PersistentFlags().Int("generate-concurrent", 6, "Number of concurrent document generation processes")
 	updateCmd.PersistentFlags().Int("post-concurrent", 4, "Number of concurrent document upload processes")
+	updateCmd.PersistentFlags().Int("duration", 1000, "Interval time[ms] for crawling.")
 
-	updateProblemCmd.Flags().Int("duration", 1000, "Duration[ms] in crawling problem")
 	updateProblemCmd.Flags().BoolP("all", "a", false, "Crawl all problems if true.")
-
-	updateUserCmd.Flags().Int("duration", 1000, "Duration[ms] in crawling problem")
 
 	updateCmd.AddCommand(updateProblemCmd)
 	updateCmd.AddCommand(updateUserCmd)
