5aff49ca3954d2b0fadf7c6dbeda4958ce37454b
fjnkt98 <fjnkt98@gmail.com>
2023-08-06 20:27:46 +0900

    add generate submit command

--- backend/cmd/generate.go
+++ backend/cmd/generate.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fjnkt98/atcodersearch/problem"
+	"fjnkt98/atcodersearch/submission"
 	"fjnkt98/atcodersearch/user"
 	"log"
 
@@ -55,11 +56,32 @@ var generateUserCmd = &cobra.Command{
 	},
 }
 
+var generateSubmissionCmd = &cobra.Command{
+	Use:   "submission",
+	Short: "Generate submission document JSON files",
+	Long:  "Generate submission document JSON files",
+	Run: func(cmd *cobra.Command, args []string) {
+		saveDir, err := GetSaveDir(cmd, "submission")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		generator := submission.NewDocumentGenerator(GetDB(), saveDir)
+		concurrent, err := cmd.Flags().GetInt("concurrent")
+		if err != nil {
+			log.Fatalf("failed to get value of `concurrent` flag: %s", err.Error())
+		}
+		if err := generator.Run(100000, concurrent); err != nil {
+			log.Fatal(err.Error())
+		}
+	},
+}
+
 func init() {
 	generateCmd.PersistentFlags().String("save-dir", "", "Directory path at which generated documents will be saved")
 	generateCmd.PersistentFlags().Int("concurrent", 10, "Concurrent number of document generation processes")
 	generateCmd.AddCommand(generateProblemCmd)
 	generateCmd.AddCommand(generateUserCmd)
+	generateCmd.AddCommand(generateSubmissionCmd)
 
 	rootCmd.AddCommand(generateCmd)
 }
