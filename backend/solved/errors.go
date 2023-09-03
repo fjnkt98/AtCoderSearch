35f09b9d78d1cf17595327f36c4492e5836f7f77
fjnkt98 <fjnkt98@gmail.com>
2023-09-03 13:08:41 +0900

    remove unnecessary core

--- backend/solved/errors.go
+++
@@ -1,9 +0,0 @@
-package solved
-
-import "github.com/morikuni/failure"
-
-const (
-	DBError            failure.StringCode = "DBError"
-	EncodeError        failure.StringCode = "EncodeError"
-	FileOperationError failure.StringCode = "FileOperationError"
-)
