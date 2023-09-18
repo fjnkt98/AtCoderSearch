78d9b703ab9eaebb0c90f6c52a5e2fa87ec09847
fjnkt98 <fjnkt98@gmail.com>
2023-09-18 19:05:52 +0900

    add migrate command to indexer

---
+++ backend/cmd/migrate.go
@@ -0,0 +1,82 @@
+package cmd
+
+import (
+	"errors"
+	"fjnkt98/atcodersearch/acs"
+	"fmt"
+	"net/url"
+	"os"
+	"strconv"
+
+	"github.com/k0kubun/sqldef"
+	"github.com/k0kubun/sqldef/database"
+	"github.com/k0kubun/sqldef/database/postgres"
+	"github.com/k0kubun/sqldef/parser"
+	"github.com/k0kubun/sqldef/schema"
+	"github.com/morikuni/failure"
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/slog"
+)
+
+func migrate(databaseURL string, schemaFile string) error {
+	u, err := url.Parse(databaseURL)
+	if err != nil {
+		return failure.Translate(err, acs.InvalidURL, failure.Context{"databaseURL": databaseURL, "schema": schemaFile}, failure.Message("invalid database url was given"))
+	}
+	password, ok := u.User.Password()
+	if !ok {
+		return failure.New(acs.InvalidURL, failure.Context{"databaseURL": databaseURL, "schema": schemaFile}, failure.Message("failed to get password from the database url"))
+	}
+
+	port, err := strconv.Atoi(u.Port())
+	if err != nil {
+		if errors.Is(err, strconv.ErrSyntax) {
+			port = 5432
+		} else {
+			return failure.Translate(err, acs.InvalidURL, failure.Context{"databaseURL": databaseURL, "schema": schemaFile}, failure.Message("failed to convert port number from given database url"))
+		}
+	}
+
+	db, err := postgres.NewDatabase(database.Config{
+		DbName:   u.Path[1:],
+		User:     u.User.Username(),
+		Password: password,
+		Host:     u.Hostname(),
+		Port:     port,
+	})
+	if err != nil {
+		return failure.Translate(err, acs.DBError, failure.Context{"databaseURL": databaseURL, "schema": schemaFile}, failure.Message("failed to create a database adapter"))
+	}
+
+	sqlParser := database.NewParser(parser.ParserModePostgres)
+	desiredDDLs, err := sqldef.ReadFile(schemaFile)
+	if err != nil {
+		return failure.Translate(err, acs.FileOperationError, failure.Context{"databaseURL": databaseURL, "schema": schemaFile}, failure.Message("failed to read schema file"))
+	}
+	options := &sqldef.Options{DesiredDDLs: desiredDDLs}
+	if u.Hostname() == "localhost" {
+		os.Setenv("PGSSLMODE", "disable")
+	}
+	sqldef.Run(schema.GeneratorModePostgres, db, sqlParser, options)
+
+	return nil
+}
+
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "Migrate database table schema",
+	Long:  "Migrate database table schema",
+	Run: func(cmd *cobra.Command, args []string) {
+		url := os.Getenv("DATABASE_URL")
+		schema := os.Getenv("DB_SCHEMA_FILE")
+		if err := migrate(url, schema); err != nil {
+			slog.Error("failed to migrate database schema", slog.String("error", fmt.Sprintf("%+v", err)))
+			os.Exit(1)
+		}
+		slog.Info("finished migrating database schema successfully.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+}
--- backend/go.mod
+++ backend/go.mod
@@ -4,13 +4,16 @@ go 1.20
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/coocood/freecache v1.2.3
 	github.com/deckarep/golang-set/v2 v2.3.0
+	github.com/gitsight/go-echo-cache v1.0.1
 	github.com/go-playground/validator/v10 v10.14.1
 	github.com/gorilla/schema v1.2.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
+	github.com/k0kubun/sqldef v0.16.7
 	github.com/labstack/echo/v4 v4.11.1
-	github.com/lib/pq v1.2.0
+	github.com/lib/pq v1.10.9
 	github.com/morikuni/failure v1.1.2
 	github.com/spf13/cobra v1.7.0
 	github.com/tdewolff/minify v2.3.6+incompatible
@@ -21,20 +24,20 @@ require (
 
 require (
 	github.com/andybalholm/cascadia v1.2.0 // indirect
-	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/coocood/freecache v1.2.3 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
-	github.com/gitsight/go-echo-cache v1.0.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/k0kubun/pp/v3 v3.2.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mcuadros/go-defaults v1.2.0 // indirect
+	github.com/pganalyze/pg_query_go/v4 v4.2.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tdewolff/parse v2.3.4+incompatible // indirect
 	github.com/tdewolff/test v1.0.9 // indirect
@@ -44,4 +47,6 @@ require (
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	google.golang.org/protobuf v1.23.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
