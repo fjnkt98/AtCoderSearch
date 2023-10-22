e311d9a50937cb10ded5510c7b5a51426a7c3804
fjnkt98 <fjnkt98@gmail.com>
2023-10-22 12:55:21 +0900

    add primary key

--- backend/schema.sql
+++ backend/schema.sql
@@ -108,7 +108,8 @@ CREATE INDEX "update_history_domain_index" ON "update_history" ("domain");
 CREATE TABLE "submission_crawl_history" (
     "id" bigserial NOT NULL,
     "contest_id" text NOT NULL,
-    "started_at" bigint NOT NULL
+    "started_at" bigint NOT NULL,
+    PRIMARY KEY ("id")
 );
 
 CREATE INDEX "submission_crawl_history_contest_id_start_at_index" ON "submission_crawl_history" ("contest_id", "started_at");
