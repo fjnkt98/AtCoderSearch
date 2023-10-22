2b185daaff737a1119d3edd20c80f0d6677f0512
fjnkt98 <fjnkt98@gmail.com>
2023-10-22 13:51:56 +0900

    ignore errors

--- frontend/src/lib/SearchBar.svelte
+++ frontend/src/lib/SearchBar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  // @ts-ignore
   import Icon from "svelte-icons-pack/Icon.svelte";
   import AiOutlineSearch from "svelte-icons-pack/ai/AiOutlineSearch";
   import HiOutlinePaperAirplane from "svelte-icons-pack/hi/HiOutlinePaperAirplane";
--- frontend/src/routes/search/submission/Filter.svelte
+++ frontend/src/routes/search/submission/Filter.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
+  // @ts-ignore
   import Icon from "svelte-icons-pack/Icon.svelte";
   import AiOutlineSearch from "svelte-icons-pack/ai/AiOutlineSearch";
   import HiOutlinePaperAirplane from "svelte-icons-pack/hi/HiOutlinePaperAirplane";
