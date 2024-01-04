033ff488fd4345e29b05bc9262c582ea3bda7ae6
fjnkt98 <fjnkt98@gmail.com>
2024-01-04 10:05:08 +0900

    change name of the database and user for test

--- backend/repository/contest_test.go
+++ backend/repository/contest_test.go
@@ -1,6 +1,6 @@
 //go:build test_repository
 
-// docker run --rm -d -p 5432:5432 --name postgres -e POSTGRES_DB=atcodersearch -e POSTGRES_USER=atcodersearch -e POSTGRES_PASSWORD=atcodersearch --mount type=bind,src=./schema.sql,dst=/docker-entrypoint-initdb.d/schema.sql postgres:15
+// docker run --rm -d -p 5432:5432 --name postgres -e POSTGRES_DB=test_atcodersearch -e POSTGRES_USER=test_atcodersearch -e POSTGRES_PASSWORD=test_atcodersearch --mount type=bind,src=./schema.sql,dst=/docker-entrypoint-initdb.d/schema.sql postgres:15
 
 package repository
 
@@ -23,7 +23,7 @@ func ptr[T any](v T) *T {
 
 func getTestDB() *bun.DB {
 	os.Setenv("PGSSLMODE", "disable")
-	engine, err := sql.Open("postgres", "postgres://atcodersearch:atcodersearch@localhost/atcodersearch")
+	engine, err := sql.Open("postgres", "postgres://test_atcodersearch:test_atcodersearch@localhost/test_atcodersearch")
 	if err != nil {
 		slog.Error("failed to open database", slog.String("error", err.Error()))
 		os.Exit(1)
