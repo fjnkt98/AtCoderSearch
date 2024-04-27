c34d45b280188b66127df65c825579421185edeb
fjnkt98 <fjnkt98@gmail.com>
2024-04-28 01:16:21 +0900

    add generate problem cmd

--- backend/cmd/generate/problem.go
+++ backend/cmd/generate/problem.go
@@ -1,13 +1,20 @@
 package generate
 
-import "github.com/urfave/cli/v2"
+import (
+	"fjnkt98/atcodersearch/batch/generate"
+	"fjnkt98/atcodersearch/repository"
+
+	"github.com/goark/errs"
+	"github.com/urfave/cli/v2"
+)
 
 func newGenerateProblemCmd() *cli.Command {
 	return &cli.Command{
 		Name: "problem",
 		Flags: []cli.Flag{
 			&cli.PathFlag{
-				Name: "save-dir",
+				Name:    "save-dir",
+				EnvVars: []string{"PROBLEM_SAVE_DIR"},
 			},
 			&cli.IntFlag{
 				Name:  "chunk-size",
@@ -18,8 +25,23 @@ func newGenerateProblemCmd() *cli.Command {
 				Value: 4,
 			},
 		},
-		Action: func(c *cli.Context) error {
-			panic(0)
+		Action: func(ctx *cli.Context) error {
+			pool, err := repository.NewPool(ctx.Context, ctx.String("database-url"))
+			if err != nil {
+				return errs.Wrap(err)
+			}
+			reader := generate.NewProblemRowReader(pool)
+			generator := generate.NewProblemGenerator(
+				reader,
+				ctx.String("save-dir"),
+				ctx.Int("chunk-size"),
+				ctx.Int("concurrent"),
+			)
+
+			if err := generator.Generate(ctx.Context); err != nil {
+				return errs.Wrap(err)
+			}
+			return nil
 		},
 	}
 }
