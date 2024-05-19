8c80f5667217d367c9528b34e4c57d6fa734ae3c
fjnkt98 <fjnkt98@gmail.com>
2024-05-19 16:16:19 +0900

    redirect to problem search page from top page forcibly

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-  import Tab from "$lib/Tab.svelte";
-  import logo from "$lib/assets/logo.svg";
   import Header from "$lib/Header.svelte";
+  import Tab from "$lib/Tab.svelte";
 </script>
 
 <div class="flex h-dvh flex-col">
   <Header />
   <Tab />
-  <div class="p-4">
-    <p>Releases</p>
-  </div>
 </div>
