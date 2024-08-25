d05c8f5e982eb5a93b03a9f5563538e5f7c7f325
fjnkt98 <fjnkt98@gmail.com>
2024-08-25 15:47:55 +0900

    register commands

--- batch/cmd/crawl.go
+++ batch/cmd/crawl.go
@@ -1,10 +1,18 @@
 package cmd
 
 import (
+	"errors"
+	"net/http"
+	"strings"
 	"time"
 
+	"github.com/fjnkt98/atcodersearch-batch/crawl"
+	"github.com/fjnkt98/atcodersearch-batch/pkg/atcoder"
+	"github.com/fjnkt98/atcodersearch-batch/repository"
 	"github.com/urfave/cli/v2"
 )
+
+var ErrEmptyAuth = errors.New("atcoder-username or atcoder-password is empty")
 
 func NewCrawlCmd() *cli.Command {
 	crawlProblemCmd := &cli.Command{
@@ -22,7 +30,41 @@ func NewCrawlCmd() *cli.Command {
 			},
 		},
 		Action: func(ctx *cli.Context) error {
-			// TODO
+			problemsClient := atcoder.NewAtCoderProblemsClientWithHTTPClient(
+				&http.Client{
+					Timeout: 5 * time.Second,
+				},
+			)
+			atcoderClient, err := atcoder.NewAtCoderClientWithHTTPClient(
+				&http.Client{
+					Timeout: 5 * time.Second,
+				},
+			)
+			if err != nil {
+				return err
+			}
+
+			pool, err := repository.NewPool(ctx.Context, ctx.String("database-url"))
+			if err != nil {
+				return err
+			}
+
+			if err := crawl.NewContestCrawler(problemsClient, pool).Crawl(ctx.Context); err != nil {
+				return err
+			}
+			if err := crawl.NewDifficultyCrawler(problemsClient, pool).Crawl(ctx.Context); err != nil {
+				return err
+			}
+			if err := crawl.NewProblemCrawler(
+				problemsClient,
+				atcoderClient,
+				pool,
+				ctx.Duration("duration"),
+				ctx.Bool("all"),
+			).Crawl(ctx.Context); err != nil {
+				return err
+			}
+
 			return nil
 		},
 	}
@@ -36,7 +78,24 @@ func NewCrawlCmd() *cli.Command {
 			},
 		},
 		Action: func(ctx *cli.Context) error {
-			// TODO
+			client, err := atcoder.NewAtCoderClientWithHTTPClient(
+				&http.Client{
+					Timeout: 5 * time.Second,
+				},
+			)
+			if err != nil {
+				return err
+			}
+
+			pool, err := repository.NewPool(ctx.Context, ctx.String("database-url"))
+			if err != nil {
+				return err
+			}
+
+			if err := crawl.NewUserCrawler(client, pool, ctx.Duration("duration")).Crawl(ctx.Context); err != nil {
+				return err
+			}
+
 			return nil
 		},
 	}
@@ -66,8 +125,47 @@ func NewCrawlCmd() *cli.Command {
 			},
 		},
 		Action: func(ctx *cli.Context) error {
+			client, err := atcoder.NewAtCoderClientWithHTTPClient(
+				&http.Client{
+					Timeout: 5 * time.Second,
+				},
+			)
+			if err != nil {
+				return err
+			}
+
+			username := ctx.String("atcoder-username")
+			password := ctx.String("atcoder-password")
+			if username == "" || password == "" {
+				return ErrEmptyAuth
+			}
+
+			if err := client.Login(ctx.Context, username, password); err != nil {
+				return err
+			}
+
+			pool, err := repository.NewPool(ctx.Context, ctx.String("database-url"))
+			if err != nil {
+				return err
+			}
+
+			var targets []string
+			if target := ctx.String("target"); target != "" {
+				targets = strings.Split(target, ",")
+			}
+
+			crawler := crawl.NewSubmissionCrawler(
+				client,
+				pool,
+				ctx.Duration("duration"),
+				ctx.Int("retry"),
+				targets,
+			)
+			if err := crawler.Crawl(ctx.Context); err != nil {
+				return err
+			}
+
 			return nil
-			// TODO
 		},
 	}
 
