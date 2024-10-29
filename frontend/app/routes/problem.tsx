044dd2a33ddc467027aa609093f40814799dc9bd
fjnkt98 <fjnkt98@gmail.com>
2024-10-29 23:09:26 +0900

    wip: filtering menu

--- frontend/app/routes/problem.tsx
+++ frontend/app/routes/problem.tsx
@@ -1,4 +1,5 @@
-import { useLoaderData, json } from "@remix-run/react";
+import { useLoaderData, json, Form } from "@remix-run/react";
+import { useState } from "react";
 import createClient from "openapi-fetch";
 import type { paths } from "~/types";
 // import {
@@ -24,23 +25,50 @@ export const loader = async () => {
 
 export default function ProblemPage() {
   const data = useLoaderData<typeof loader>();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggle = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <div className="">
-      <div className="flex flex-row justify-between items-center py-2 px-2">
+      <div
+        className={`${
+          menuOpen ? "block" : "hidden"
+        } absolute w-5/6 border border-blue-500 bg-white dark:bg-gray-950 px-4 py-2`}
+      >
+        <Form method="GET">
+          <div className="flex flex-row items-center justify-between text-lg">
+            <span>絞り込み</span>
+            <button type="button" onClick={toggle}>
+              ✗
+            </button>
+          </div>
+          <p>キーワード</p>
+          <input type="text" name="q" />
+
+          <button type="submit">絞り込む</button>
+          <button type="button">クリア</button>
+        </Form>
+      </div>
+      <div className="flex flex-row justify-between lg:justify-end items-center py-2 px-2">
         <button
           type="button"
-          className="rounded-xl px-3 py-1 border shadow-sm shadow-gray-500"
+          className="lg:hidden rounded-xl px-3 py-1 border shadow-sm shadow-gray-500"
+          onClick={toggle}
         >
           絞り込み
         </button>
-        <span>約1,000件 / 30ms</span>
+        <span>
+          約{data.total}件 / {data.time}ms
+        </span>
       </div>
       <div className="flex flex-col items-center px-2 text-sm gap-1 py-1">
         {data.items.map((item) => (
           <div
             key={item.problemId}
-            className="border rounded-md min-w-60 w-full py-2 flex flex-row items-center justify-between gap-2 px-2"
+            className="border rounded-md min-w-60 max-w-xl w-full py-2 flex flex-row items-center justify-between gap-2 px-2"
           >
             <a href={item.contestUrl} className="text-center">
               {item.contestId}
