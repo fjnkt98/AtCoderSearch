160c7563a252510f51de935a15b2f567069a4a0d
fjnkt98 <fjnkt98@gmail.com>
2024-11-04 16:43:13 +0900

    add submission count to user documents

--- backend/update/user.go
+++ backend/update/user.go
@@ -56,6 +56,8 @@ func (ix *UserIndexer) Settings() *meilisearch.Settings {
 			"birthYear",
 			"userId",
 			"rank",
+			"submissionCount",
+			"accepted",
 		},
 		Synonyms: map[string][]string{},
 	}
@@ -81,7 +83,22 @@ func NewUserRowReader(pool *pgxpool.Pool) *UserRowReader {
 
 func (r *UserRowReader) ReadRows(ctx context.Context, tx chan<- UserRow) error {
 	db := bun.NewDB(stdlib.OpenDBFromPool(r.pool), pgdialect.New())
+	submissionCounts := db.NewSelect().
+		ColumnExpr("user_id").
+		ColumnExpr("COUNT(*) AS submission_count").
+		TableExpr("submissions").
+		GroupExpr("user_id")
+
+	accepts := db.NewSelect().
+		ColumnExpr("user_id").
+		ColumnExpr("COUNT(DISTINCT problem_id) AS accepted").
+		TableExpr("submissions").
+		Where("result = ?", "AC").
+		GroupExpr("user_id")
+
 	rows, err := db.NewSelect().
+		With("sc", submissionCounts).
+		With("ac", accepts).
 		ColumnExpr("u.user_id").
 		ColumnExpr("u.rating").
 		ColumnExpr("u.highest_rating").
@@ -93,7 +110,11 @@ func (r *UserRowReader) ReadRows(ctx context.Context, tx chan<- UserRow) error {
 		ColumnExpr("u.rank").
 		ColumnExpr("u.active_rank").
 		ColumnExpr("u.wins").
+		ColumnExpr("sc.submission_count").
+		ColumnExpr("ac.accepted").
 		TableExpr("users AS u").
+		Join("LEFT JOIN sc ON u.user_id = sc.user_id").
+		Join("LEFT JOIN ac ON u.user_id = ac.user_id").
 		Rows(ctx)
 
 	if err != nil {
@@ -118,17 +139,19 @@ func (r *UserRowReader) ReadRows(ctx context.Context, tx chan<- UserRow) error {
 }
 
 type UserRow struct {
-	UserID        string  `bun:"user_id"`
-	Rating        int     `bun:"rating"`
-	HighestRating int     `bun:"highest_rating"`
-	Affiliation   *string `bun:"affiliation"`
-	BirthYear     *int    `bun:"birth_year"`
-	Country       *string `bun:"country"`
-	Crown         *string `bun:"crown"`
-	JoinCount     int     `bun:"join_count"`
-	Rank          int     `bun:"rank"`
-	ActiveRank    *int    `bun:"active_rank"`
-	Wins          int     `bun:"wins"`
+	UserID          string  `bun:"user_id"`
+	Rating          int     `bun:"rating"`
+	HighestRating   int     `bun:"highest_rating"`
+	Affiliation     *string `bun:"affiliation"`
+	BirthYear       *int    `bun:"birth_year"`
+	Country         *string `bun:"country"`
+	Crown           *string `bun:"crown"`
+	JoinCount       int     `bun:"join_count"`
+	Rank            int     `bun:"rank"`
+	ActiveRank      *int    `bun:"active_rank"`
+	Wins            int     `bun:"wins"`
+	SubmissionCount int     `bun:"submission_count"`
+	Accepted        int     `bun:"accepted"`
 }
 
 func (r UserRow) Document(ctx context.Context) (UserDocument, error) {
@@ -159,38 +182,42 @@ func (r UserRow) Document(ctx context.Context) (UserDocument, error) {
 	}
 
 	return UserDocument{
-		UserID:         r.UserID,
-		Rating:         r.Rating,
-		RatingFacet:    ratingFacet,
-		HighestRating:  r.HighestRating,
-		Affiliation:    r.Affiliation,
-		BirthYear:      r.BirthYear,
-		BirthYearFacet: birthYearFacet,
-		Country:        r.Country,
-		Crown:          r.Crown,
-		JoinCount:      r.JoinCount,
-		JoinCountFacet: joinCountFacet,
-		Rank:           r.Rank,
-		ActiveRank:     r.ActiveRank,
-		Wins:           r.Wins,
-		UserURL:        fmt.Sprintf("https://atcoder.jp/users/%s", r.UserID),
+		UserID:          r.UserID,
+		Rating:          r.Rating,
+		RatingFacet:     ratingFacet,
+		HighestRating:   r.HighestRating,
+		Affiliation:     r.Affiliation,
+		BirthYear:       r.BirthYear,
+		BirthYearFacet:  birthYearFacet,
+		Country:         r.Country,
+		Crown:           r.Crown,
+		JoinCount:       r.JoinCount,
+		JoinCountFacet:  joinCountFacet,
+		Rank:            r.Rank,
+		ActiveRank:      r.ActiveRank,
+		Wins:            r.Wins,
+		UserURL:         fmt.Sprintf("https://atcoder.jp/users/%s", r.UserID),
+		SubmissionCount: r.SubmissionCount,
+		Accepted:        r.Accepted,
 	}, nil
 }
 
 type UserDocument struct {
-	UserID         string  `json:"userId"`
-	Rating         int     `json:"rating"`
-	RatingFacet    string  `json:"ratingFacet,omitempty"`
-	HighestRating  int     `json:"highestRating"`
-	Affiliation    *string `json:"affiliation"`
-	BirthYear      *int    `json:"birthYear"`
-	BirthYearFacet string  `json:"birthYearFacet,omitempty"`
-	Country        *string `json:"country"`
-	Crown          *string `json:"crown"`
-	JoinCount      int     `json:"joinCount"`
-	JoinCountFacet string  `json:"joinCountFacet,omitempty"`
-	Rank           int     `json:"rank"`
-	ActiveRank     *int    `json:"activeRank"`
-	Wins           int     `json:"wins" `
-	UserURL        string  `json:"userUrl"`
+	UserID          string  `json:"userId"`
+	Rating          int     `json:"rating"`
+	RatingFacet     string  `json:"ratingFacet,omitempty"`
+	HighestRating   int     `json:"highestRating"`
+	Affiliation     *string `json:"affiliation"`
+	BirthYear       *int    `json:"birthYear"`
+	BirthYearFacet  string  `json:"birthYearFacet,omitempty"`
+	Country         *string `json:"country"`
+	Crown           *string `json:"crown"`
+	JoinCount       int     `json:"joinCount"`
+	JoinCountFacet  string  `json:"joinCountFacet,omitempty"`
+	Rank            int     `json:"rank"`
+	ActiveRank      *int    `json:"activeRank"`
+	Wins            int     `json:"wins" `
+	UserURL         string  `json:"userUrl"`
+	Accepted        int     `json:"accepted"`
+	SubmissionCount int     `json:"submissionCount"`
 }
--- backend/update/user_test.go
+++ backend/update/user_test.go
@@ -2,6 +2,9 @@ package update
 
 import (
 	"context"
+	"fjnkt98/atcodersearch/internal/testutil"
+	"fjnkt98/atcodersearch/repository"
+	"path/filepath"
 	"reflect"
 	"testing"
 
@@ -18,6 +21,7 @@ func TestUserDocument(t *testing.T) {
 		{name: "lower bound", user: UserRow{Rating: 0, BirthYear: ptr.To(1970), JoinCount: 0}, want: UserDocument{Rating: 0, RatingFacet: "   0 ~  400", BirthYear: ptr.To(1970), BirthYearFacet: "1970 ~ 1980", JoinCount: 0, JoinCountFacet: "   0 ~   20", UserURL: "https://atcoder.jp/users/"}},
 		{name: "upper bound", user: UserRow{Rating: 3999, BirthYear: ptr.To(2019), JoinCount: 99}, want: UserDocument{Rating: 3999, RatingFacet: "3600 ~ 4000", BirthYear: ptr.To(2019), BirthYearFacet: "2010 ~ 2020", JoinCount: 99, JoinCountFacet: "  80 ~  100", UserURL: "https://atcoder.jp/users/"}},
 		{name: "greater than", user: UserRow{Rating: 4000, BirthYear: ptr.To(2020), JoinCount: 100}, want: UserDocument{Rating: 4000, RatingFacet: "4000 ~     ", BirthYear: ptr.To(2020), BirthYearFacet: "2020 ~     ", JoinCount: 100, JoinCountFacet: " 100 ~     ", UserURL: "https://atcoder.jp/users/"}},
+		{name: "counts", user: UserRow{UserID: "fjnkt98", Rating: 1123, HighestRating: 1123, Affiliation: nil, BirthYear: ptr.To(1998), Country: ptr.To("JP"), Crown: ptr.To("user-green-4"), JoinCount: 40, Rank: 18728, ActiveRank: ptr.To(10256), Wins: 0, SubmissionCount: 2, Accepted: 1}, want: UserDocument{UserID: "fjnkt98", Rating: 1123, RatingFacet: " 800 ~ 1200", HighestRating: 1123, Affiliation: nil, BirthYear: ptr.To(1998), BirthYearFacet: "1990 ~ 2000", Country: ptr.To("JP"), Crown: ptr.To("user-green-4"), JoinCount: 40, JoinCountFacet: "  40 ~   60", Rank: 18728, ActiveRank: ptr.To(10256), Wins: 0, UserURL: "https://atcoder.jp/users/fjnkt98", Accepted: 1, SubmissionCount: 2}},
 	}
 
 	ctx := context.Background()
@@ -33,5 +37,70 @@ func TestUserDocument(t *testing.T) {
 				t.Errorf("expect %+v, but got %+v", tt.want, actual)
 			}
 		})
+	}
+}
+
+func TestReadUserRows(t *testing.T) {
+	matches, err := filepath.Glob("./testdata/*.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+	files := make([]string, len(matches))
+	for i, m := range matches {
+		file, err := filepath.Abs(m)
+		if err != nil {
+			t.Fatal(err)
+		}
+		files[i] = file
+	}
+
+	ctx := context.Background()
+
+	_, dsn, stopDB, err := testutil.CreateDBContainer(files...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pool, err := repository.NewPool(ctx, dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		stopDB()
+	})
+
+	reader := NewUserRowReader(pool)
+
+	ch := make(chan UserRow, 10)
+	if err := reader.ReadRows(ctx, ch); err != nil {
+		t.Error(err)
+	}
+	close(ch)
+
+	rows := make([]UserRow, 0)
+	for row := range ch {
+		rows = append(rows, row)
+	}
+
+	want := []UserRow{
+		{
+			UserID:          "fjnkt98",
+			Rating:          1123,
+			HighestRating:   1123,
+			Affiliation:     nil,
+			BirthYear:       ptr.To(1998),
+			Country:         ptr.To("JP"),
+			Crown:           ptr.To("user-green-4"),
+			JoinCount:       40,
+			Rank:            18728,
+			ActiveRank:      ptr.To(10256),
+			Wins:            0,
+			SubmissionCount: 2,
+			Accepted:        1,
+		},
+	}
+
+	if !reflect.DeepEqual(want, rows) {
+		t.Errorf("expected %+v, but got %+v", want, rows)
 	}
 }
